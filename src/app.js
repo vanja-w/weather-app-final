0b8bef05e9d708d2eb210fd84fb5160f6cd954f5
vanja-w <vanja.wallace@gmail.com>
2022-01-28 16:20:44 +0100

    Local time
    formateDateTime() function receives a parameter named timezone which passes through the timezone data (response.data.timezone) fetched from the openweathermap.org API.
    As the timezone data represents a shift in seconds from UTC, it needs to be divided by 3600 in order to convert the timezone seconds into hours. Also, instead of the getHours() method, getUTCHours() was used to calculate the local time of any given location in respect of UTC time and not user's local time.
    When getHours() is used, the difference between user's local and UTC time needs to be added or subtracted in the calculation because this method returns the hours in the specified date according to local time. Obviously, this is not a good practice as the difference between one user's local time and the UTC is not the same for any other user.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,49 @@
+function formatDateTime(timezone) {
+  let currentDate = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+
+  let date = currentDate.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentDate.getMonth()];
+
+  let year = currentDate.getFullYear();
+
+  let hours = currentDate.getUTCHours() + timezone / 3600;
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${date} ${month} ${year} | ${hours}:${minutes}`;
+}
+
 function displayWeatherData(response) {
   console.log(response.data);
   let locationElement = document.querySelector("#location-name");
@@ -10,8 +56,10 @@ function displayWeatherData(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let weatherDescription = document.querySelector("#weather-description");
   weatherDescription.innerHTML = response.data.weather[0].description;
+  let dateTimeElement = document.querySelector("#date-time");
+  dateTimeElement.innerHTML = formatDateTime(response.data.timezone);
 }
 
 let apiKey = "c904083ce9d848d6eee6931b635cd191";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Honolulu&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeatherData);
